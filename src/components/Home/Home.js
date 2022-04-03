9cfabf61275f79e4d3280a3ff0fadabaac53ce79
RAKIBUL HASAN <rakibul15-13011@diu.edu.bd>
2022-04-04 00:01:56 +0600

    navber part done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+             <h1 className='text-6xl'>Your next Air Buds</h1>
+            <h1 className='text-6xl'>Your Best Air Buds</h1>
+            <h1>This is home</h1>
+        </div>
+    );
+};
+
+export default Home;
